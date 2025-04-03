ac3a7e1afdb807a7031182cb034be43863fa0aa4
Satyam Kumar <113851151+SKumr20@users.noreply.github.com>
2025-04-03 13:48:02 +0530

    Add Margin for Select course/quiz title

--- src/app/select-quiz/conservation-econ/page.jsx
+++ src/app/select-quiz/conservation-econ/page.jsx
@@ -3,7 +3,7 @@ import { AuroraText } from "@/components/magicui/aurora-text";
 export default function ConservationPage() {
 
     return (
-        <div className="flex flex-col justify-center items-center mt-10">
+        <div className="flex flex-col justify-center items-center mt-14">
           <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
               Select <AuroraText>Week: </AuroraText>
           </h1>
--- src/app/select-quiz/page.jsx
+++ src/app/select-quiz/page.jsx
@@ -4,7 +4,7 @@ import { AuroraText } from "@/components/magicui/aurora-text";
 export default function SelectQuiz() {
 
     return (
-        <div className="flex flex-col justify-center items-center mt-10">
+        <div className="flex flex-col justify-center items-center mt-14">
             <h1 className="text-4xl font-bold tracking-tighter md:text-5xl lg:text-7xl">
                 Select <AuroraText>Course: </AuroraText>
             </h1>
